480c7226b4b87b6be2685a7482698ddd93c3aae4
Brexblime <jaouhari.bahaadine@gmail.com>
2023-06-25 17:57:53 +0100

    update

--- sort.h
+++ sort.h
@@ -15,9 +15,9 @@
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
 
@@ -29,9 +29,4 @@ void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 
 
-
-
-
-
-
-#endif
+#endif /* SORT_H */
